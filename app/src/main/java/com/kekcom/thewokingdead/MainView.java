dd3f0fe76757151978a44c63d15e64d46ca3b80c
Marcus <mar.hernandez010@gmail.com>
2017-07-29 16:58:25 -0700

    MainUI Done

--- app/src/main/java/com/kekcom/thewokingdead/MainView.java
+++ app/src/main/java/com/kekcom/thewokingdead/MainView.java
@@ -1,8 +1,24 @@
 package com.kekcom.thewokingdead;
 
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import android.app.Activity;
+import android.view.MenuItem;
+import android.util.DisplayMetrics;
+
+import android.view.Menu;
+import android.content.Intent;
+import android.view.MenuInflater;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.view.Window;
+
 /**
  * Created by Marcus on 7/29/2017.
  */
 
-public class MainView {
+public class MainView extends SurfaceView implements SurfaceHolder.Callback{
+
 }
