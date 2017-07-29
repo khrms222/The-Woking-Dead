96b57e590af140c123a418fa0d96b712754356cf
Marcus <mar.hernandez010@gmail.com>
2017-07-29 16:02:20 -0700

    Main Activity

--- app/src/main/java/com/kekcom/thewokingdead/MainActivity.java
+++ app/src/main/java/com/kekcom/thewokingdead/MainActivity.java
@@ -3,11 +3,29 @@ package com.kekcom.thewokingdead;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMain = (Button) findViewById(R.id.buttonMain);
+
+        buttonMain.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, Start.class);
+
+                beginPlay(i);
+            }
+        });
     }
 }
