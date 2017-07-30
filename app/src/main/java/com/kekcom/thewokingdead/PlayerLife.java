4975c87f50c98457a2197a38ed2dbbcfc9feec15
Ken Hoac <ken.hoac@gmail.com>
2017-07-29 19:19:59 -0700

    Added WeaponObject and PlayerLife

---
+++ app/src/main/java/com/kekcom/thewokingdead/PlayerLife.java
@@ -0,0 +1,10 @@
+package com.kekcom.thewokingdead;
+
+import android.content.Context;
+
+public class PlayerLife extends GameObject{
+
+    public PlayerLife(Context context, int drawable) {
+        super(context, drawable);
+    }
+}
