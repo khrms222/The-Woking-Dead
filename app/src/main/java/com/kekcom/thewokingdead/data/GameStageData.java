a503358eda7b4be1b883638cf72c11a9df765d94
Double-Lu <lujacky375@gmail.com>
2017-07-30 19:48:26 -0700

    predetermined spawn

--- app/src/main/java/com/kekcom/thewokingdead/data/GameStageData.java
+++ app/src/main/java/com/kekcom/thewokingdead/data/GameStageData.java
@@ -72,16 +72,16 @@ public class GameStageData extends GameDAO {
                     check1 = false;
                 }
             }
-            boolean check2 = true;
-            while (check2) {
-                int x = (int) (Math.random() * size);
-                int y = (int) (Math.random() * size);
-                if (gw[x][y] == "00") {
-                    a = x;
-                    b = y;
-                    check2 = false;
-                }
-            }
+//            boolean check2 = true;
+//            while (check2) {
+//                int x = (int) (Math.random() * size);
+//                int y = (int) (Math.random() * size);
+//                if (gw[x][y] == "00") {
+//                    a = x;
+//                    b = y;
+//                    check2 = false;
+//                }
+//            }
             String command = "INSERT INTO " + GameStageData.TABLE_NAME + " VALUES "
                     + "(null," + FIELD_ID_STAGE + "," + k + "," + a + "," + b + ",\"";
             for (int i = 0; i < size; i++) {
